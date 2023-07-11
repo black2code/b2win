3c5c0f3c115d04bbd47d5935da136db5dd7bbb12
black2code <alsdnr1080@gmail.com>
2023-07-11 19:34:00 +0900

    Add: refreshtoken 구현

---
+++ src/main/java/com/example/b2win/refreshtoken/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package com.example.b2win.refreshtoken.service;
+
+import com.example.b2win.refreshtoken.domain.RefreshToken;
+import com.example.b2win.refreshtoken.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
