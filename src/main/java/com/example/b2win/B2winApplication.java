b436df0cff2943574af0d5816bf380310535b4b0
black2code <alsdnr1080@gmail.com>
2023-07-11 14:51:51 +0900

    setting commit

---
+++ src/main/java/com/example/b2win/B2winApplication.java
@@ -0,0 +1,13 @@
+package com.example.b2win;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2winApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2winApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/b2win/B2winApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b2win;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2winApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
