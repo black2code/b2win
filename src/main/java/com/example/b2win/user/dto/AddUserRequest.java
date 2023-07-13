1cd060435a762f2934e1ead863755ddf1768186e
black2code <alsdnr1080@gmail.com>
2023-07-13 20:25:43 +0900

    Add: 어노테이션 정리

--- src/main/java/com/example/b2win/user/dto/AddUserRequest.java
+++ src/main/java/com/example/b2win/user/dto/AddUserRequest.java
@@ -1,10 +1,13 @@
 package com.example.b2win.user.dto;
 
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
