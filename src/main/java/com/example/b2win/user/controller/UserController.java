78fd1d7afd689aadf026637cc3398e38fec01b18
black2code <alsdnr1080@gmail.com>
2023-07-13 20:28:08 +0900

    Add: login 기능 구현

--- src/main/java/com/example/b2win/user/controller/UserController.java
+++ src/main/java/com/example/b2win/user/controller/UserController.java
@@ -1,31 +1,53 @@
 package com.example.b2win.user.controller;
 
+import com.example.b2win.config.jwt.TokenProvider;
+import com.example.b2win.global.dto.GlobalResDto;
+import com.example.b2win.refreshtoken.dto.TokenDto;
+import com.example.b2win.user.domain.User;
 import com.example.b2win.user.dto.AddUserRequest;
+import com.example.b2win.user.dto.LoginRequest;
+import com.example.b2win.user.mapper.UserMapperClass;
 import com.example.b2win.user.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
+    private final UserMapperClass mapper;
 
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody @Valid AddUserRequest request) {
-        userService.createUser(request);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Operation(summary = "회원 가입 api")
+    public ResponseEntity<GlobalResDto> signUp(@RequestBody @Valid AddUserRequest request) {
+        User user = mapper.userCreateDtoToUser(request);
+        userService.createUser(user);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new GlobalResDto("Success signUp", HttpStatus.CREATED.value()));
     }
 
-    @DeleteMapping("/user")
-    public ResponseEntity deleteUser(HttpServletRequest request) {
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    @PostMapping("/login")
+    @Operation(summary = "로그인 api")
+    public ResponseEntity<GlobalResDto> login(@RequestBody @Valid LoginRequest request) {
+        TokenDto tokenDto = userService.login(request);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(TokenProvider.ACCESS_TOKEN, tokenDto.getAccessToken())
+                .header(TokenProvider.REFRESH_TOKEN, tokenDto.getRefreshToken())
+                .body(new GlobalResDto("Success Login", HttpStatus.OK.value()));
     }
+
+//    @DeleteMapping("/user")
+//    @ApiOperation(value = "유저 탈퇴")
+//    public ResponseEntity deleteUser(HttpServletRequest request) {
+//        return new ResponseEntity(HttpStatus.NO_CONTENT);
+//    }
 }
--- src/main/java/com/example/b2win/user/service/UserService.java
+++ src/main/java/com/example/b2win/user/service/UserService.java
@@ -1,11 +1,22 @@
 package com.example.b2win.user.service;
 
-import com.example.b2win.user.dto.AddUserRequest;
+
+import com.example.b2win.config.jwt.TokenProvider;
+import com.example.b2win.global.error.ErrorCode;
+import com.example.b2win.refreshtoken.domain.RefreshToken;
+import com.example.b2win.refreshtoken.dto.TokenDto;
+import com.example.b2win.refreshtoken.repository.RefreshTokenRepository;
+import com.example.b2win.user.dto.LoginRequest;
 import com.example.b2win.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import com.example.b2win.user.domain.User;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
 
 @RequiredArgsConstructor
 @Service
@@ -13,15 +24,19 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
-    public void createUser(AddUserRequest dto) {
-        User user = userRepository.save(User.builder()
-                .name(dto.getName())
-                .birthday(dto.getBirthday())
-                .sex(dto.getSex())
-                .account(dto.getAccount())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
-                .email(dto.getEmail())
+
+    @Transactional
+    public void createUser(User user) {
+        userRepository.save(User.builder()
+                .name(user.getName())
+                .birthday(user.getBirthday())
+                .sex(user.getSex())
+                .account(user.getAccount())
+                .password(bCryptPasswordEncoder.encode(user.getPassword()))
+                .email(user.getEmail())
                 .build());
     }
 
@@ -29,4 +44,45 @@ public class UserService {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
+
+    @Transactional(noRollbackFor = BadCredentialsException.class)
+    public TokenDto login(LoginRequest request) {
+
+        User user = userRepository.findByAccount(request.getAccount())
+                .orElseThrow(() -> new IllegalArgumentException("Not found Account"));
+
+        if (user.isLocked()) {
+            throw new IllegalArgumentException("Account locked");
+        }
+        if (!bCryptPasswordEncoder.matches(request.getPassword(), user.getPassword())) {
+            user.incrementPasswordErrorCount();
+            if (user.getPasswordErrorCount() >= 5) {
+                user.lockAccount();
+            }
+            userRepository.save(user);
+            throw new BadCredentialsException(ErrorCode.NOTEQUAL_INPUT_PASSWORD.getDetail());
+        }
+
+        user.resetPasswordErrorCount();
+        user.unlockAccount();
+        userRepository.save(user);
+
+        if (user.isPasswordExpired()) {
+            throw new IllegalArgumentException("Password has expired");
+        }
+
+        TokenDto tokenDto = tokenProvider.createAllToken(user);
+
+        Optional <RefreshToken> refreshToken = refreshTokenRepository.findByUserId(user.getId());
+
+        if (refreshToken.isPresent()) {
+            refreshTokenRepository.save(refreshToken.get().update(tokenDto.getRefreshToken()));
+        } else {
+            RefreshToken newToken = new RefreshToken(user.getId(), tokenDto.getRefreshToken());
+            refreshTokenRepository.save(newToken);
+        }
+
+        return tokenDto;
+    }
+
 }
